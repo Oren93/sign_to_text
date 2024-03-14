e0fa2e7a6ecc868e0cb83f00e4f75329f92a8026
Karen Lam <karenlamky99@gmail.com>
2024-03-14 11:14:56 +0000

    Rearrange File

---
+++ sign-to-text-interface/serving/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+
+UPLOAD_DIR = Path() / "uploads"
+
+app = FastAPI()
+
+@app.post('/uploadfile/')
+async def create_upload_file(file_upload: UploadFile):
+    data = await file_upload.read()
+    save_to = UPLOAD_DIR / file_upload.filename
+    with open(save_to, "wb") as f:
+        f.write(data)
+
+    ## Do something to the file
+    
+    return {"filename": file_upload.filename}
