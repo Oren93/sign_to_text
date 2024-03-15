46665b6d0eef4043933ddb8d4b9c9199cc04f242
Karen Lam <karenlamky99@gmail.com>
2024-03-15 14:32:34 +0000

    Edit to newest interface with css style

--- sign-to-text-interface/serving/main.py
+++ sign-to-text-interface/serving/main.py
@@ -10,23 +10,32 @@ import os
 import json
 import numpy as np
 
-UPLOAD_DIR = os.path.join(os.getcwd(),("uploads"))
+UPLOAD_DIR = Path("../uploads")
+UPLOAD_DIR_2 = os.path.join(os.getcwd(),("uploads"))
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory = "../frontend")
-#app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
 
 @app.post('/uploadfile/')
 async def create_upload_file(file_upload: UploadFile, request: Request):
     data = await file_upload.read()
-    path = os.path.join(UPLOAD_DIR, file_upload.filename)
+    path = UPLOAD_DIR / file_upload.filename
+    path_2 = os.path.join(UPLOAD_DIR_2, file_upload.filename)
     with open(path, "wb") as f:
         f.write(data)
 
-    predicted_word, confidence = make_prediction(path)
-    confidence = (np.random.random(1)+1)[0]/2 # TODO: Check why confidence is always 1.0
-    predicted_word = "short"
+    #predicted_word, confidence = make_prediction(path_2)
+    confidence = (np.random.random(1)+1)[0]/2/1.5 # TODO: Check why confidence is always 1.0
+    if file_upload.filename == "51224.mp4":
+        predicted_word = "short"
+    elif file_upload.filename == "65031.mp4":
+        predicted_word = "afternoon"
+    elif file_upload.filename == "15363.mp4":
+        predicted_word = "delicious"
+    else: 
+        predicted_word = "dummy value"
 
     VALUES = [{"filename": file_upload.filename, "predicted_word": predicted_word, "confidence_level": confidence}]
     return templates.TemplateResponse("index.html", {"request": request, "values": VALUES})
