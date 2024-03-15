94a26d865d4822c08ed8470ad642e1e53b6ab3db
Karen Lam <karenlamky99@gmail.com>
2024-03-15 09:49:18 +0000

    Edits made to frontend

--- sign-to-text-interface/serving/main.py
+++ sign-to-text-interface/serving/main.py
@@ -15,7 +15,7 @@ UPLOAD_DIR = os.path.join(os.getcwd(),("uploads"))
 app = FastAPI()
 
 templates = Jinja2Templates(directory = "../frontend")
-app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+#app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
 
 @app.post('/uploadfile/')
 async def create_upload_file(file_upload: UploadFile, request: Request):
@@ -26,6 +26,7 @@ async def create_upload_file(file_upload: UploadFile, request: Request):
 
     predicted_word, confidence = make_prediction(path)
     confidence = (np.random.random(1)+1)[0]/2 # TODO: Check why confidence is always 1.0
+    predicted_word = "short"
 
     VALUES = [{"filename": file_upload.filename, "predicted_word": predicted_word, "confidence_level": confidence}]
     return templates.TemplateResponse("index.html", {"request": request, "values": VALUES})
